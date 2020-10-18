798abb11350568074f9d43496622cb419b907282
Kristina <cyberkitty628@gmail.com>
2020-10-18 16:23:04 -0700

    Fixed HTML, added more CSS styles, started JavaScript for quiz feature

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # letslearndutch
-My project for Intensive 1.1.
+
+Let's Learn Dutch is the tile of my project for Intensive 1.1. This is a web-based application that will provide the user with interactive tools to learn basic Dutch.
---
+++ script.js
@@ -0,0 +1,77 @@
+function displayQuiz(){
+// Variable to store HTML quiz output
+    const output= [];
+
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            // Variable stores list of answers
+            const answers = [];
+            for(letter in currentQuestion.answers){
+                // Adds HTML radio button to each answer
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            // Adds question and answer to output
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+    quizContainer.innerHTML = output.join('');
+}
+
+function showResults(){}
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [
+    {
+        question: "In Dutch, what is the correct way to refer to someone who uses she/her pronouns?",
+        answers: {
+            a: "Zij, ze, haar",
+            b: "Zij, ze, hem",
+            c: "Hij, ze, haar"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which of these phrases would you use to greet a Dutch person in the afternoon?",
+        answers: {
+            a: "Goedenavond",
+            b: "Goedemorgen",
+            c: "Goedemiddag"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the Dutch word for 'dog'?",
+        answers: {
+            a: "Paard",
+            b: "Haan",
+            c: "Hond"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which of these word combinations correctly describe the four seasons (Winter, Spring, Summer, Fall) in Dutch?",
+        answers: {
+            a: "Winter, Lente, Sommer, Herfst",
+            b: "Winter, Lente, Zomer, Herfst",
+            c: "Winter, Lente, Zomer, Herbst"
+        },
+        correctAnswer: "b"
+    }
+]
+
+// Displays quiz 
+displayQuiz();
+
+// Shows results when user clicks submit button
+submitButton.addEventListener('click', showResults);
